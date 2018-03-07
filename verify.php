dd56d5038660ee9a2c6f6e2899c37b7a20fa6c6d
taggaddaaaa <scaizerg@student.42.fr>
2018-03-08 00:45:13 +0100

    Code Manicure - clean files for next work on it. - fix bad links to some images. - Replace id's in index.html by proper name

--- verify.php
+++ verify.php
@@ -3,30 +3,22 @@
 //Formulaire de contact
 if(!empty($_POST))
 {
-    //si le prenom est vide
 	if(empty($_POST["name"])){
-        // ajouter une erreur au tableau $error
 		array_push($errors, "veuillez saisir votre nom");
 	}
 
-    //si le telephone est vide
 	if(empty($_POST["phone"])){
-        // ajouter une erreur au tableau $error
 		array_push($errors, "veuillez rentrer votre téléphone");
 	}
 
-    //si l email est vide
 	if(empty($_POST["email"])){
-        // ajouter une erreur au tableau $error
 		array_push($errors, "veuillez rentrer votre e.mail");
 	}
 	else if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)==FALSE){
 		array_push($errors, "email non valide");
 	}
 
-    // si le message est vide
 	if(empty($_POST["message"])){
-        // ajouter une erreur au tableau
 		array_push($errors, "veuillez rentrer un message");
 	}
 
@@ -47,9 +39,7 @@ if(!empty($_POST))
         $email_body .= "Email du contact : $email_address\n";
         $email_body .= "Tel. du contact : $phone\n\n\n";
         $email_body .= "Message : \n\n $message";
-        //$headers = "From : $email_address\r\n";
 		$headers = "From : $email_address\r\n" . "Reply-To: $email_address\r\n" . "X-Mailer: PHP/" . phpversion();
-
 
 		$result = mail($to, $email_subject, $email_body, $headers);
 
