12541f2274f40f2b224aa1024e900614e6c6c7e5
taggaddaaaa <scaizerg@student.42.fr>
2019-05-16 12:50:54 +0200

    move sticky btn from pages

--- index.php
+++ index.php
@@ -612,13 +612,7 @@
 <!------------------------------------------->
 <!-- BUTTON STICK ON BOTTOM ON MOBILE SIZE -->
 <!------------------------------------------->
-<div class="navbar-buttons mbr-section-btn mbr-section-btn-page">
-    <a class="btn btn-sm btn-primary btn-book-flight display-4 btn-page"
-       style="font-family: Fugaz One, sans-serif; color: #0d0d0d;"
-       href="https://www.weezevent.com/widget_billeterie.php?id_evenement=310469&lg_billetterie=1&code=66219&width_auto=1&color_primary=00AEEF"
-       onclick="var w=window.open('https://www.weezevent.com/widget_billeterie.php?id_evenement=310469&lg_billetterie=1&code=66219&width_auto=1&color_primary=00AEEF', 'Billetterie_weezevent', 'width=650, height=600, top=100, left=100, toolbar=no, resizable=yes, scrollbars=yes, status=no'); w.focus(); return false;"
-    >RESERVER UN VOL</a>
-</div>
+<?php include "./php/partials/stickyBtn.php"?>
 
 <!------------------------------------------->
 <!--                MODALS                 -->
